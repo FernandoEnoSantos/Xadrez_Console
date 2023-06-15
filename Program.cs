222f45f6b52a0a818465217c81d047a606741cc3
Fernando Barbosa Santos <muribinhalive@gmail.com>
2023-06-14 21:57:50 -0300

    Projeto iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrez_console{
+    class Program{
+        static void Main(string[] args){
+
+
+        }
+    }
+}
