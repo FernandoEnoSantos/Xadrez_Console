c32ee1b12e5c78571ad2d0bf925827e61e32df85
Fernando Barbosa Santos <muribinhalive@gmail.com>
2023-06-14 22:10:48 -0300

    Criação da Classe Posição

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Tabuleiro;
 
 namespace xadrez_console{
     class Program{
         static void Main(string[] args){
+
+
+            Posicao P = new Posicao(3,4);
+
+            System.Console.WriteLine("Posição: "+P);
+
+
 
 
         }
