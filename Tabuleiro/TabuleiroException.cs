95971881a2515fa0cca6b91733f9e91145b7afa3
Fernando Barbosa Santos <muribinhalive@gmail.com>
2023-06-16 00:48:58 -0300

    Inserção de tratamento de excecões personalizadas

---
+++ Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace tabuleiro
+{
+    public class TabuleiroException: Exception
+    {
+        
+        public TabuleiroException(string msg):base(msg){
+            
+        }
+
+
+
+
+    }
+}
