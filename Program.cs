566f8bbb7ec69ad9bb04452cdd542ddc471e23d7
Fernando Barbosa Santos <muribinhalive@gmail.com>
2023-06-17 00:07:58 -0300

    Melhorando a implementacao do metodo imprimir tabuleiro

--- Program.cs
+++ Program.cs
@@ -8,12 +8,25 @@ namespace Xadrez_Console{
     class Program{
         static void Main(string[] args){
 
+            try {
+                Tabuleiro tab = new Tabuleiro(8,8);
 
-            PosicaoXadrez pos = new PosicaoXadrez('c',7);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1,3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0,2));
+                
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3,5));
 
-            System.Console.WriteLine(pos);
 
-            System.Console.WriteLine(pos.toPosicao());
+                Tela.imprimirTabuleiro(tab);
+
+
+            }
+            catch(TabuleiroException e){
+
+                System.Console.WriteLine(e.Message);
+
+            }
 
 
         }
--- Tela.cs
+++ Tela.cs
@@ -11,7 +11,11 @@ namespace Xadrez_Console
         public static void imprimirTabuleiro(Tabuleiro tab){
 
             for(int i = 0; i < tab.linhas ; i++){
+                System.Console.Write(8-i+ " ");
+
                 for(int j = 0; j < tab.colunas;j++){
+                
+
 
                 if(tab.peca(i,j) == null){
 
@@ -20,18 +24,38 @@ namespace Xadrez_Console
                 }
                 else{
 
-                    System.Console.Write(tab.peca(i,j)+ " ");
-
+                    imprimirPeca(tab.peca(i,j));
+                    System.Console.Write(" ");
                 }      
 
                 }
+                
 
                 System.Console.WriteLine();  
 
 
             }
+            System.Console.WriteLine("  a b c d e f g h");
 
 
+        }
+
+        public static void imprimirPeca(Peca peca){
+            if(peca.cor == Cor.Branca){
+
+                System.Console.Write(peca);
+
+
+            }
+            else{
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                System.Console.Write(peca);
+                Console.ForegroundColor = aux;
+                // macete para mudar a cor do console para amarelo, armazenar a cor original em auxiliar
+                // imprimir amarelo e depois voltar a cor original
+
+            }
 
         }
 
