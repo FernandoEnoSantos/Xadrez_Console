49bd5ec2e525ac0fd9a1fd20f5e5c9831b71990f
Fernando Barbosa Santos <muribinhalive@gmail.com>
2023-06-21 13:09:06 -0300

    Jogada especial roque pequeno e grande

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -35,6 +35,27 @@ namespace xadrez
                 capturadas.Add(pecaCapturada);
 
             }
+
+            //#jogada especial Roque pequeno
+            if(p is Rei && destino.coluna == origem.coluna +2){
+                Posicao origemT = new Posicao(origem.linha, origem.coluna+3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna+1);
+                Peca T = tab.retirarPeca(origemT);
+                T.incrementarQteMovimentos();
+                tab.colocarPeca(T, destinoT);
+
+            }
+
+            //#jogada especial Roque grande
+            if(p is Rei && destino.coluna == origem.coluna - 2){
+                Posicao origemT = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(origemT);
+                T.incrementarQteMovimentos();
+                tab.colocarPeca(T, destinoT);
+
+            }
+
             return pecaCapturada;
 
         }
@@ -49,6 +70,26 @@ namespace xadrez
 
             }
             tab.colocarPeca(p,origem);
+
+            //#jogada especial Roque pequeno
+            if(p is Rei && destino.coluna == origem.coluna +2){
+                Posicao origemT = new Posicao(origem.linha, origem.coluna+3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna+1);
+                Peca T = tab.retirarPeca(destinoT);
+                T.decrementarQteMovimentos();
+                tab.colocarPeca(T, origemT);
+
+            }
+
+            //#jogada especial Roque grande
+            if(p is Rei && destino.coluna == origem.coluna - 2){
+                Posicao origemT = new Posicao(origem.linha, origem.coluna-4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna-1);
+                Peca T = tab.retirarPeca(destinoT);
+                T.decrementarQteMovimentos();
+                tab.colocarPeca(T, origemT);
+
+            }
 
         }
 
@@ -250,9 +291,9 @@ namespace xadrez
             colocarNovaPeca('b',1,new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('c',1,new Bispo(tab, Cor.Branca));
             colocarNovaPeca('d',1,new Dama(tab, Cor.Branca));
-            colocarNovaPeca('e',1,new Rei(tab, Cor.Branca));
-            colocarNovaPeca('f',1,new Bispo(tab, Cor.Branca));
-            colocarNovaPeca('g',1,new Cavalo(tab, Cor.Branca));
+            colocarNovaPeca('e',1,new Rei(tab, Cor.Branca, this)); // como está dentro da classe partida
+            colocarNovaPeca('f',1,new Bispo(tab, Cor.Branca));      // e o construtor do rei preve na instanciação um argumento de partida
+            colocarNovaPeca('g',1,new Cavalo(tab, Cor.Branca));     //colocamos "this" para autoreferenciar nesse caso o atributo partida...
             colocarNovaPeca('h',1,new Torre(tab, Cor.Branca));
             colocarNovaPeca('a',2,new Peao(tab, Cor.Branca));
             colocarNovaPeca('b',2,new Peao(tab, Cor.Branca));
@@ -267,7 +308,7 @@ namespace xadrez
             colocarNovaPeca('b',8,new Cavalo(tab, Cor.Preta));
             colocarNovaPeca('c',8,new Bispo(tab, Cor.Preta));
             colocarNovaPeca('d',8,new Dama(tab, Cor.Preta));
-            colocarNovaPeca('e',8,new Rei(tab, Cor.Preta));
+            colocarNovaPeca('e',8,new Rei(tab, Cor.Preta,this));
             colocarNovaPeca('f',8,new Bispo(tab, Cor.Preta));
             colocarNovaPeca('g',8,new Cavalo(tab, Cor.Preta));
             colocarNovaPeca('h',8,new Torre(tab, Cor.Preta));
--- xadrez/Rei.cs
+++ xadrez/Rei.cs
@@ -4,8 +4,11 @@ namespace xadrez
 {
     public class Rei: Peca
     {
-        
-        public Rei(Tabuleiro tab, Cor cor): base (tab,cor){
+        private PartidaDeXadrez partida; // o rei precisa ter acesso a partida para verificar se não está em xeque
+        public Rei(Tabuleiro tab, Cor cor, PartidaDeXadrez partida): base (tab,cor){
+
+            this.partida = partida;
+
         }
 
         public override string ToString()
@@ -17,6 +20,12 @@ namespace xadrez
             Peca p = tab.peca(pos);
             return p == null || p.cor != cor; 
 
+
+        }
+        
+        private bool testeTorreParaRoque(Posicao pos){
+            Peca p = tab.peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.qtMovimentos == 0;
 
         }
 
@@ -81,6 +90,31 @@ namespace xadrez
             if(tab.posicaoValida(pos) && podeMover(pos)){
                 mat[pos.linha, pos.coluna] = true;
             }
+
+            //#jogada especial: Roque
+
+            if(qtMovimentos == 0 && !partida.xeque){
+                // Roque pequeno
+                Posicao posT1 = new Posicao(posicao.linha, posicao.coluna +3);
+                if(testeTorreParaRoque(posT1)){
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna +1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna +2);
+                    if(tab.peca(p1) == null && tab.peca(p2) == null){
+                        mat[posicao.linha, posicao.coluna +2 ] = true;
+                    }
+                }
+                //Roque grande
+                Posicao posT2 = new Posicao(posicao.linha, posicao.coluna - 4);
+                if(testeTorreParaRoque(posT2)){
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna -1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna -2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna -3);
+                    if(tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3) == null){
+                        mat[posicao.linha, posicao.coluna - 2 ] = true;
+                    }
+                }
+            }
+            
 
 
             return mat;
