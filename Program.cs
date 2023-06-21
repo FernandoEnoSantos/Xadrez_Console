367dae13ef6b7b9f3f4619ff5acf9e95b693bf63
Fernando Barbosa Santos <muribinhalive@gmail.com>
2023-06-20 21:28:56 -0300

    Implementando as restricoes de movimento

--- Program.cs
+++ Program.cs
@@ -12,12 +12,18 @@ namespace Xadrez_Console{
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
                 while(!partida.terminada){
+
+                    try{
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
+                    System.Console.WriteLine();
+                    System.Console.WriteLine("Turno: "+partida.turno);
+                    System.Console.WriteLine("Aguardando jogada: "+ partida.jogadorAtual);
                     
                     System.Console.WriteLine();
                     System.Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    partida.validarPosicaoDeOrigem(origem);
 
                     bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
@@ -28,9 +34,17 @@ namespace Xadrez_Console{
                     System.Console.WriteLine();
                     System.Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                    partida.validarPosicaoDeDestino(origem,destino);
 
-                    partida.executaMovimento(origem, destino);
+                    partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e){
 
+                        System.Console.WriteLine(e.Message);
+                        Console.ReadLine();// apertar enter e esperar o jogador repetir a jogada!
+
+
+                    }
 
                 }
 
--- Tabuleiro/Peca.cs
+++ Tabuleiro/Peca.cs
@@ -24,6 +24,33 @@ namespace tabuleiro
 
         }
 
+        public bool existeMovimentosPossiveis(){
+            bool[,] mat = movimentosPossiveis();
+
+            for(int i = 0; i<tab.linhas; i++){
+                for(int j = 0; j<tab.colunas; j++){
+
+                    if(mat[i,j])// mesma coisa que mat[i,j] == true pois o IF verifica se é verdadeira a existencia dele
+                    {
+                        return true; // return corta o método
+
+
+                    }
+                }
+
+
+            }
+            return false;
+
+        }
+
+        public bool podeMoverPara(Posicao pos){
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+
+
+        }
+
+
         public abstract bool[,] movimentosPossiveis();
 
 
