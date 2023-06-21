11e4ae595e99a60e2a640a59c26bfd291d3aedef
Fernando Barbosa Santos <muribinhalive@gmail.com>
2023-06-21 14:45:25 -0300

    jogada especial en passant

--- xadrez/Peao.cs
+++ xadrez/Peao.cs
@@ -4,7 +4,11 @@ namespace xadrez
 {
     public class Peao:Peca
     {
-        public Peao(Tabuleiro tab, Cor cor): base (tab,cor){
+
+      private PartidaDeXadrez partida;
+        public Peao(Tabuleiro tab, Cor cor,PartidaDeXadrez partida): base (tab,cor){
+         this.partida = partida;
+
         }
 
         public override string ToString()
@@ -51,6 +55,25 @@ namespace xadrez
                    mat[pos.linha, pos.coluna] = true;
                 }
 
+                //#jogada especial en passant Brancas
+
+                if(posicao.linha == 3){
+                  Posicao esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
+                     if(tab.posicaoValida(esquerda) && existeInimigo(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant){
+
+                        mat[esquerda.linha - 1,esquerda.coluna] = true;
+
+                     }
+
+                  Posicao direita = new Posicao(posicao.linha, posicao.coluna + 1);
+                     if(tab.posicaoValida(direita ) && existeInimigo(direita ) && tab.peca(direita ) == partida.vulneravelEnPassant){
+
+                        mat[direita.linha - 1, direita.coluna] = true;
+
+                     }
+
+                }
+
             }
             else{
                 pos.definirValores(posicao.linha + 1, posicao.coluna);
@@ -73,10 +96,30 @@ namespace xadrez
                    mat[pos.linha, pos.coluna] = true;
                 }
 
+                //#jogada especial en passant Pretas
+
+                if(posicao.linha == 4){
+                  Posicao esquerda = new Posicao(posicao.linha, posicao.coluna - 1);
+                     if(tab.posicaoValida(esquerda) && existeInimigo(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant){
+
+                        mat[esquerda.linha + 1,esquerda.coluna] = true;
+
+                     }
+
+                  Posicao direita = new Posicao(posicao.linha, posicao.coluna + 1);
+                     if(tab.posicaoValida(direita ) && existeInimigo(direita ) && tab.peca(direita ) == partida.vulneravelEnPassant){
+
+                        mat[direita.linha + 1, direita.coluna] = true;
+
+                     }
+
+                }
 
 
 
             }
+
+
 
 
             return mat;
